3dff8ead61705ee800172fb3a89743ae328fc96c
naom1029 <56006010+naom1029@users.noreply.github.com>
2026-03-21 14:34:31 +0900

    perf: DOMContentLoadedで即マウント＋ルート別チャンクprefetch

--- application/client/src/index.tsx
+++ application/client/src/index.tsx
@@ -3,8 +3,36 @@ import { BrowserRouter } from "react-router";
 
 import { AppContainer } from "@web-speed-hackathon-2026/client/src/containers/AppContainer";
 
-createRoot(document.getElementById("app")!).render(
-  <BrowserRouter>
-    <AppContainer />
-  </BrowserRouter>,
-);
+// Route-level prefetch: start loading the chunk for the current page early
+if (/^\/posts\/[^/?#]+/.test(window.location.pathname)) {
+  void import("@web-speed-hackathon-2026/client/src/containers/PostContainer");
+}
+if (window.location.pathname === "/search") {
+  void import("@web-speed-hackathon-2026/client/src/containers/SearchContainer");
+}
+if (/^\/terms\/?$/.test(window.location.pathname)) {
+  void import("@web-speed-hackathon-2026/client/src/containers/TermContainer");
+}
+if (/^\/crok\/?$/.test(window.location.pathname)) {
+  void import("@web-speed-hackathon-2026/client/src/containers/CrokContainer");
+}
+if (/^\/dm\/?$/.test(window.location.pathname)) {
+  void import("@web-speed-hackathon-2026/client/src/containers/DirectMessageListContainer");
+}
+if (/^\/dm\/[^/?#]+/.test(window.location.pathname)) {
+  void import("@web-speed-hackathon-2026/client/src/containers/DirectMessageContainer");
+}
+
+const mount = () => {
+  createRoot(document.getElementById("app")!).render(
+    <BrowserRouter>
+      <AppContainer />
+    </BrowserRouter>,
+  );
+};
+
+if (document.readyState === "loading") {
+  window.addEventListener("DOMContentLoaded", mount, { once: true });
+} else {
+  mount();
+}
